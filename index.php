26aa13663d4fc1040afe888dd600d573f029895d
marines-casadaslinhas <mariness354@gmail.com>
2019-10-19 11:23:40 -0300

    alteracoes

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+    header("Location: ./home/index.php");
+    require_once "config/constantes.php";
+
+    // $u = $_GET['url'];
+
+    // $arrayU = explode("/", $u);
+
+
+//     if(count($arrayU) == 3) {
+//         $url = "localhost/casaDasLinhas/".$arrayU[0]."/".$arrayU[1].".php?id=".$arrayU[2];
+// //        echo $url;
+// //        die;
+//         header("'Location: ".$url."'");
+//     } else {
+//         $url = "localhost/casaDasLinhas/".$_GET['url'].".php";
+// //        echo $url;
+// //        die;
+//         header('Location: localhost/casaDasLinhas/produtos/pesquisar.php');
+//     }
+
+
+
+
+
+?>
+

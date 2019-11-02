c4f73ad2d65d762017fc40a30a7fee8cc73ca986
marines-casadaslinhas <mariness354@gmail.com>
2019-11-02 11:03:23 -0300

    alteracoes

--- importdb.php
+++ importdb.php
@@ -1,6 +1,36 @@
 <?php
+	$linhaArray = Array();
+	$i = 0;
+	$file = fopen('./arquivos/EstoquePHP.csv', 'r');
+	while (($line = fgetcsv($file)) !== false)
+	{
+  		if($line[0] != ';;') {
+	  		$linhaArray[] = $line;
+	  	} 
+	}
+	fclose($file);
 
+	foreach ($linhaArray as $linha) {
+			
+		if(count($linha) > 1) {
+			$linha[0] = $linha[0]. ' ' . $linha[1];
+			unset($linha[1]);
+		}
 
+		$linha = explode(';', $linha[0]);
 
+		if(isset($linha[2]) && !empty($linha[2])) {
+			$linha[2] = str_replace('.', '', $linha[2]);
+		}	
 
+		if(isset($linha[2]) && empty($linha[2])) { 
+			$linha[2] = 1;
+		}
+
+		echo '<pre>';
+			print_r($linha);
+		echo '</pre>';	
+	}
+	
+	die;
 ?>

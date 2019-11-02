0f3850c11b504793d630b44f5cb90d1f49b8ad5d
Maiquel Leites <maiquelveve2008@gmail.com>
2019-11-02 12:54:08 -0300

    alteracoes

--- importdb.php
+++ importdb.php
@@ -19,14 +19,43 @@
 
 		$linha = explode(';', $linha[0]);
 
+		/*****************************************************************************/
+
+		/*AJUSTANDO O NOME DO PRODUTO*/
+		//Aqui retira os espaços em branco entre as palavras e deixa só um
+		$linha[0] = preg_replace('/\s+/', ' ', $linha[0]);			
+
+		//Apaga o numero no inicio do nome caso ele exista
+		$nomeAux = explode(' ', $linha[0]);
+		if(is_numeric($nomeAux[0])) {
+			$newName = "";
+			for($i = 1; $i < count($nomeAux); $i++){
+				$newName .= $nomeAux[$i]. ' ';
+			}
+
+			$linha[0] = $newName;
+		} 
+
+		/*****************************************************************************/
+
+		/*AJUSTANDO O CODIGO DE BARRA*/
+		//Limpa o código de barras
 		if(isset($linha[2]) && !empty($linha[2])) {
 			$linha[2] = str_replace('.', '', $linha[2]);
 		}	
-
+		//Se não possuir código de barras ele atribui 1
 		if(isset($linha[2]) && empty($linha[2])) { 
 			$linha[2] = 1;
 		}
 
+		/*****************************************************************************/
+
+		/*AJUSTANDO O TAMANHO*/
+		$linha[9] = explode(' ', $linha[0]); 
+
+		/*****************************************************************************/
+
+		/* SAIDA */
 		echo '<pre>';
 			print_r($linha);
 		echo '</pre>';	
